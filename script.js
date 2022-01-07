7b99cec9ae47b9a52fe566f1cf9028dfdf5780e9
Nischal <nischal.aremanda1432@gmail.com>
2022-01-07 17:42:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    document.getElementById(
+      "demo").innerHTML = "A JavaScript object is a collection of properties, and a property is an association between a name (or key) and a value.. ";
+}
